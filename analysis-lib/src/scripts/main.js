6fa5e12659bdd0c148d2b85e1f51716b59a6e560
Geraldo Ribeiro Franciscani <gfranciscani@gmail.com>
2018-01-25 14:16:36 -0200

    Applies filter to values read from the db

--- analysis-lib/src/scripts/main.js
+++ analysis-lib/src/scripts/main.js
@@ -1,17 +1,21 @@
 // Run all filters
-let loadTsv = require('./load-tsv');
 let filters = require('../filters/filters-module');
 let reportToolModule = require('./report-tool');
 let argv = require('minimist')(process.argv.slice(2));
+let Promise = require('bluebird')
 
-let inputDir = ['data-tests', 'input-files'];
-let inputFile = argv.input;
+// DB modules
+let dbInfo = require('../db-scripts/db-info');
+let dbInterface = require('../db-scripts/db-interface-module');
+let sqlite = require('sqlite');
 
-// Retrieves filter name
-let filterName = `filter${argv.filter}`;
+// Filter string
+let filterRegex = /(filter)[0-9]+/i;
+let filterId = argv['filter'];
+let filterName = filterRegex.test(filterId) ? filterId : `filter${filterId}`;
 
 // Run all filters if no filter was specified or does not exist
-let runAll = !argv.filter;
+let runAll = !argv['filter'];
 
 // checks if chosen filter exists
 let found = false;
@@ -24,17 +28,98 @@ if(!found && !runAll) {
   throw(`\n***** ${filterName} does not exists. Try a different <filter_id> *****\n`);
 }
 
-// Initializes report for given tsv file
-let report = new reportToolModule();
-report.init(inputFile);
+// Retrieves dataset id
+let datasetId = argv['input'];
 
 // No filter specified
-if(!inputFile) {
-  throw("No input file specified");
+if(!datasetId) {
+  throw("No datasetID specified");
 }
 
+// Initialize DB Interface
+let dbName = 'analysis-lib';
+let dbPath = dbInfo[dbName]['path'].concat(dbInfo[dbName]['name']).join('/');
+let tsvFilesTable = dbInfo[dbName]['tables']['tsv_files'].name;
+// let dbInterface = new dbInterfaceModule().loadDB(dbPath);
+
+// Initializes report for given tsv file
+let report = new reportToolModule();
+
+try {
+
+  let db;
+  let tsvTableName;
+  let noOfRows = 0;
+
+  let dbPromise = Promise.resolve()
+    .then(() => sqlite.open(dbPath, { Promise }))
+    .then(database => {
+      // stashes this promise for further use
+      db = database;
+      return db;
+    })
+    .then(() => db.get(`SELECT table_name FROM ${tsvFilesTable} WHERE rowID = ${datasetId}`))
+    .then(result => tsvTableName = result['table_name'])
+    .then(() => report.init(tsvTableName))
+    .then(() => {
+
+      // Loads file
+      db.all(`SELECT * FROM ${tsvTableName}`)
+        .then((rows) => {
+
+          noOfRows += 1;
+
+          rows.forEach((row, idx) => {
+
+            // Runs all filters
+            if(runAll) {
+
+              Object.keys(filters).forEach(filter => {
+
+                report.addFilter(filter);
+                filters[filter](row, idx, report);
+
+              });
+
+            }
+
+            // Run single filter
+            else {
+
+              report.addFilter(filterName);
+              filters[filterName](row, idx, report);
+
+            }
+
+          });
+
+        })
+        .then(() => {
+          report.saveNoOfRows(noOfRows);
+
+          // report.calcDatasetMetadata(1);
+          // report.printFilterReport('filter1');
+          report.calcFieldByFieldReport(filterId);
+
+          // report.calcDatasetMetadataAll(1);
+          // report.printReport(1);
+
+          // report.printReport();
+          // report.printFilterReport(1);
+
+        });
+
+    });
+
+}
+
+catch (err) {
+
+  next(err);
+
+}
 // Joins full path
-inputFile = inputDir.concat(inputFile).join("/");
+// inputFile = inputDir.concat(inputFile).join("/");
 
 // Gets stream object
 // let stream = loadTsv(inputFile);
