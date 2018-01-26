9f6d2b65f34f11d988f9b4bbd90407a39b89e973
success0312 <webdev0902@gmail.com>
2018-01-26 23:32:44 +0100

    refactor

--- electron-app/src/renderer/components/pages/FieldLevelPage.vue
+++ electron-app/src/renderer/components/pages/FieldLevelPage.vue
@@ -1,166 +1,166 @@
 <template lang="pug">
 include _mixins
 #barba-wrapper
-	.page(class=outClass)
-		block header
-			AppHeader
-		.page__content
-			block content
-				.p-container
-					.container
-						.p-container__wrapper
-							.container.container--smaller
+    .page(class=outClass)
+        block header
+            AppHeader
+        .page__content
+            block content
+                .p-container
+                    .container
+                        .p-container__wrapper
+                            .container.container--smaller
 
-								router-link(:to="'/submissions'").page-back
-									.icon.icon-arrow-back
-									span Report Summary
-						
-								.p-box.report
-									// summary
-									.report-summary
-										.report-summary__col
-											.report-summary__head risk analysis
-											.report-summary__text.report-summary__text--red Errors Per Row 
-										.report-summary__label.report-summary__label--red failed
-										.report-summary__col
-											.report-summary__head batch id
-											.report-summary__text 1020
-										.report-summary__col
-											.report-summary__head batch
-											.report-summary__text Monday June 12th 2017 11:00AM PST
-										.report-summary__col
-											.report-summary__head download
-											a(href="#").report-summary__text
-												+icon('ico-download')
-									
-									// table
-									table.p-table.p-table--full(js-stacktable)
-										thead
-											tr
-												td #
-												td Criteria ID and Link
-												td Short Explanation
-												td Test Data Row ID
-												td Test Data Field IDs & Values
-												
-										tbody
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td Criteria ID and Link
-												td Short Explanation
-												td Test Data Row ID
-												td Test Data Field IDs & Values
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 123152
-												td keywords match
-												td 554
-												td 000242
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 12312412
-												td muscial, music for relax
-												td 554
-												td 00054
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 5245
-												td keywords match
-												td 554
-												td 00000231
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 23123
-												td sould, deep sound
-												td 554
-												td 2232323
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 998
-												td keywords match
-												td 234
-												td 00423
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td Criteria ID and Link
-												td Short Explanation
-												td Test Data Row ID
-												td Test Data Field IDs & Values
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 123152
-												td keywords match
-												td 554
-												td 000242
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 12312412
-												td muscial, music for relax
-												td 554
-												td 00054
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 5245
-												td keywords match
-												td 554
-												td 00000231
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 23123
-												td sould, deep sound
-												td 554
-												td 2232323
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 998
-												td keywords match
-												td 234
-												td 00423
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td Criteria ID and Link
-												td Short Explanation
-												td Test Data Row ID
-												td Test Data Field IDs & Values
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 123152
-												td keywords match
-												td 554
-												td 000242
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 12312412
-												td muscial, music for relax
-												td 554
-												td 00054
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 5245
-												td keywords match
-												td 554
-												td 00000231
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 23123
-												td sould, deep sound
-												td 554
-												td 2232323
-											tr(js-modal data-mfp-src='#modal-1')
-												td #
-												td 998
-												td keywords match
-												td 234
-												td 00423
-								
-								// more btn
-								.p-container__more
-									a(href="#" js-load-more).btn.btn-more
-										span Load more
+                                router-link(:to="'/submissions'").page-back
+                                    .icon.icon-arrow-back
+                                    span Report Summary
+                        
+                                .p-box.report
+                                    // summary
+                                    .report-summary
+                                        .report-summary__col
+                                            .report-summary__head risk analysis
+                                            .report-summary__text.report-summary__text--red Errors Per Row 
+                                        .report-summary__label.report-summary__label--red failed
+                                        .report-summary__col
+                                            .report-summary__head batch id
+                                            .report-summary__text 1020
+                                        .report-summary__col
+                                            .report-summary__head batch
+                                            .report-summary__text Monday June 12th 2017 11:00AM PST
+                                        .report-summary__col
+                                            .report-summary__head download
+                                            a(href="#").report-summary__text
+                                                +icon('ico-download')
+                                    
+                                    // table
+                                    table.p-table.p-table--full(js-stacktable)
+                                        thead
+                                            tr
+                                                td #
+                                                td Criteria ID and Link
+                                                td Short Explanation
+                                                td Test Data Row ID
+                                                td Test Data Field IDs & Values
+                                                
+                                        tbody
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td Criteria ID and Link
+                                                td Short Explanation
+                                                td Test Data Row ID
+                                                td Test Data Field IDs & Values
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 123152
+                                                td keywords match
+                                                td 554
+                                                td 000242
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 12312412
+                                                td muscial, music for relax
+                                                td 554
+                                                td 00054
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 5245
+                                                td keywords match
+                                                td 554
+                                                td 00000231
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 23123
+                                                td sould, deep sound
+                                                td 554
+                                                td 2232323
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 998
+                                                td keywords match
+                                                td 234
+                                                td 00423
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td Criteria ID and Link
+                                                td Short Explanation
+                                                td Test Data Row ID
+                                                td Test Data Field IDs & Values
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 123152
+                                                td keywords match
+                                                td 554
+                                                td 000242
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 12312412
+                                                td muscial, music for relax
+                                                td 554
+                                                td 00054
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 5245
+                                                td keywords match
+                                                td 554
+                                                td 00000231
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 23123
+                                                td sould, deep sound
+                                                td 554
+                                                td 2232323
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 998
+                                                td keywords match
+                                                td 234
+                                                td 00423
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td Criteria ID and Link
+                                                td Short Explanation
+                                                td Test Data Row ID
+                                                td Test Data Field IDs & Values
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 123152
+                                                td keywords match
+                                                td 554
+                                                td 000242
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 12312412
+                                                td muscial, music for relax
+                                                td 554
+                                                td 00054
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 5245
+                                                td keywords match
+                                                td 554
+                                                td 00000231
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 23123
+                                                td sould, deep sound
+                                                td 554
+                                                td 2232323
+                                            tr(js-modal data-mfp-src='#modal-1')
+                                                td #
+                                                td 998
+                                                td keywords match
+                                                td 234
+                                                td 00423
+                                
+                                // more btn
+                                .p-container__more
+                                    a(href="#" js-load-more).btn.btn-more
+                                        span Load more
 
-				//- include components/_modal
-		block footer
-			AppFooter
+                //- include components/_modal
+        block footer
+            AppFooter
 </template>
 
 <script>
