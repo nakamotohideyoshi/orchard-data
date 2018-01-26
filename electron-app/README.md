82544b4694488153cf288c5d63efd4c1c23f8f2d
success0312 <webdev0902@gmail.com>
2018-01-26 23:06:08 +0100

    import analysis module

---
+++ electron-app/README.md
@@ -0,0 +1,9 @@
+## How to install
+
+npm install -g vue-cli
+npm install 
+npm install postinstall:deps
+
+## How to run
+
+npm run dev
--- electron-app/src/main/index.js
+++ electron-app/src/main/index.js
@@ -2,6 +2,7 @@
 
 import { app, BrowserWindow } from 'electron'
 
+const analysisLibModule = require('../../../analysis-lib/analysis-lib-module');
 // stubbing out - causes unit test error
 // import {initialize} from '../db.js'
 
