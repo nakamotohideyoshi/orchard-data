030c3286ef060138d664b6679cb4436580809dc9
success0312 <webdev0902@gmail.com>
2018-01-30 15:37:44 +0100

    refactor

--- electron-app/test/unit/specs/FieldLevelPage.spec.js
+++ electron-app/test/unit/specs/FieldLevelPage.spec.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import FieldLevelPage from '@/components/pages/FieldLevelPage'
 
 describe('FieldLevelPage.vue', () => {
   it('should render correct header', () => {
+    Vue.use(VueRouter)
     const Constructor = Vue.extend(FieldLevelPage);
     const FieldLevelPageComponent = new Constructor().$mount();
     expect(FieldLevelPageComponent.$el.textContent).to.contain('batch')
   })
 
   it('should render correct contents', () => {
+    Vue.use(VueRouter)
     const Constructor = Vue.extend(FieldLevelPage);
     const FieldLevelPageComponent = new Constructor().$mount();
     const date = new Date()
