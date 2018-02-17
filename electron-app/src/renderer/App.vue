60ff46d26d2aa32dd8bd1e048db80d12ff4b77e4
Jaime Tanori <jaime.tanori@gmail.com>
2018-02-16 18:03:33 -0800

    #60 Add config store types, fetch config data on app startup

--- electron-app/src/renderer/App.vue
+++ electron-app/src/renderer/App.vue
@@ -1,13 +1,31 @@
 
 <template>
-  <div id="app">
-     <router-view></router-view>
+  <div id="app" :class="{loading: !loaded}">
+    <!-- We can easily add a splash screen with this class while the config is loading -->
+     <router-view v-if="loaded"></router-view>
+     <!-- TODO: Add fail recovery if the API returns an error -->
+     <div v-if="error">error</div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import {
+  FILTERS_META,
+  CONFIG_FAILURE
+} from '@/constants/types'
+
   export default {
-    name: 'app'
+    name: 'app',
+    computed: {
+      ...mapGetters({
+        loaded: FILTERS_META,
+        error: CONFIG_FAILURE
+      })
+    },
+    async created() {
+      await this.$store.dispatch('getConfig');
+    }
   }
 </script>
 
