bc0b3973f27fdbb1f18f83f99707ccc8b993d5a1
Jaime Tanori <jaime.tanori@gmail.com>
2018-02-16 18:03:56 -0800

    #60 Show field level details

--- electron-app/src/renderer/store/modules/Submissions.js
+++ electron-app/src/renderer/store/modules/Submissions.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 import {
     SUBMISSIONS,
     SUBMISSION,
@@ -10,8 +10,8 @@ import {
     FIELDS_REQUEST
 } from '@/constants/types';
 import {
-  API_URL
-} from '@/constants/config'
+    API_URL
+} from '@/constants/config';
 
 const state = {
     [`${SUBMISSIONS}`]: [],
@@ -24,18 +24,18 @@ const state = {
 }
 
 const mutations = {
-  [`${SUBMISSION}`] (s, data) {
-    return Object.assign(s, { [`${SUBMISSION}`]: data })
-  },
-  [`${SUBMISSIONS}`] (s, items) {
-    if (items instanceof Array && items.length) {
-      return Object.assign(s, { [`${SUBMISSIONS}`]: items })
-    }
+    [`${SUBMISSION}`](s, data) {
+        return Object.assign(s, { [`${SUBMISSION}`]: data });
+    },
+    [`${SUBMISSIONS}`](s, items) {
+        if(items instanceof Array && items.length) {
+            return Object.assign(s, { [`${SUBMISSIONS}`]: items });
+        }
 
-    return Object.assign(s, { [`${SUBMISSIONS}`]: [] })
-  },
-  [`${SUBMISSIONS_REQUEST}`] (s, status) {
-    let finalStatus = false
+        return Object.assign(s, { [`${SUBMISSIONS}`]: [] });
+    },
+    [`${SUBMISSIONS_REQUEST}`](s, status) {
+        let finalStatus = false;
 
         if(status) {
             finalStatus = true;
@@ -73,81 +73,54 @@ const mutations = {
 }
 
 const actions = {
-  fetchSubmissions ({ commit }) {
-    commit(SUBMISSIONS_REQUEST, true)
+    fetchSubmissions ({ commit }) {
+        commit(SUBMISSIONS_REQUEST, true);
 
-    axios
-      .get(`${API_URL}dataset-meta-all`, {
-        'headers': {
-          'content-type': 'application/json'
-        }
-      })
-      .then((res) => {
-        commit(SUBMISSIONS_REQUEST, false)
-        commit(SUBMISSIONS, res.data)
-      })
-      .catch((e) => {
-        commit(SUBMISSIONS_REQUEST, false)
-        commit(SUBMISSIONS_FAILURE, e)
-      })
-  },
-  submitDataset ({ commit }, data) {
-    commit(SUBMISSIONS_REQUEST, true)
-    commit(SUBMISSIONS_FAILURE, false) // RESET IN CASE OF A RE-TRY
+        axios
+            .get(`${API_URL}dataset-meta-all`, {
+                'headers': {
+                    'content-type': 'application/json'
+                }
+            })
+            .then((res) => {
+                commit(SUBMISSIONS_REQUEST, false);
+                commit(SUBMISSIONS, res.data);
+            })
+            .catch((e) => {
+                commit(SUBMISSIONS_REQUEST, false);
+                commit(SUBMISSIONS_FAILURE, e);
+            });
+    },
+    submitDataset ({ commit }, data) {
+        commit(SUBMISSIONS_REQUEST, true);
+        commit(SUBMISSIONS_FAILURE, false); // RESET IN CASE OF A RE-TRY
 
-    return axios
-      .post(`${API_URL}dataset`, data, {
-        // If this is a constant then we should configure axios
-        // to always use it unless otherwise is especified
-        'headers': {
-          'content-type': 'application/json'
-        }
-      })
-      .then((res) => {
-        const item = res.data
+        return axios
+            .post(`${API_URL}dataset`, data, {
+                // If this is a constant then we should configure axios
+                // to always use it unless otherwise is especified
+                'headers': {
+                    'content-type': 'application/json'
+                }
+            })
+            .then((res) => {
+                const item = res.data;
 
-        commit(SUBMISSIONS_REQUEST, false)
-        commit(SUBMISSIONS_FAILURE, null)
-        commit(SUBMISSION, item)
-        commit(SUBMISSIONS_ADD, item)
-      })
-      .catch((e) => {
-        commit(SUBMISSIONS_REQUEST, false)
-        commit(SUBMISSIONS_FAILURE, e)
-      })
-  },
-  fetchDataset ({ commit }, id) {
-    commit(SUBMISSIONS_REQUEST, true)
-    commit(SUBMISSIONS_FAILURE, null)
-    commit(SUBMISSION, null)
+                commit(SUBMISSIONS_REQUEST, false);
+                commit(SUBMISSIONS_FAILURE, null);
+                commit(SUBMISSION, item);
+                commit(SUBMISSIONS_ADD, item);
+            })
+            .catch((e) => {
+                commit(SUBMISSIONS_REQUEST, false);
+                commit(SUBMISSIONS_FAILURE, e);
+            });
+    },
+    fetchDataset ({ commit }, id) {
+        commit(SUBMISSIONS_REQUEST, true);
+        commit(SUBMISSIONS_FAILURE, null);
+        commit(SUBMISSION, null);
 
-<<<<<<< b7ceee8ebe63cf9fe4f1cc69ac2272bca8679067
-    return axios
-      .get(`${API_URL}dataset-meta/${id}`, {
-        'headers': {
-          'content-type': 'application/json'
-        }
-      })
-      .then((res) => {
-        commit(SUBMISSIONS_REQUEST, false)
-        // Not sure about this, will this endpoint ever return more
-        // than one result?
-        commit(SUBMISSION, res.data[0])
-      })
-      .catch((e) => {
-        commit(SUBMISSIONS_REQUEST, false)
-        commit(SUBMISSIONS_FAILURE, e)
-      })
-  }
-}
-
-const getters = {
-  [`${SUBMISSION}`]: s => s[SUBMISSION],
-  [`${SUBMISSIONS}`]: s => s[SUBMISSIONS],
-  [`${SUBMISSIONS_REQUEST}`]: s => s[SUBMISSIONS_REQUEST],
-  [`${SUBMISSIONS_FAILURE}`]: s => s[SUBMISSIONS_FAILURE]
-}
-=======
         return axios
             .get(`${API_URL}dataset-meta/${id}`, {
                 'headers': {
@@ -201,11 +174,10 @@ const getters = {
     [`${FIELDS_REQUEST}`]: s => s[FIELDS_REQUEST],
     [`${FIELDS_FAILURE}`]: s => s[FIELDS_FAILURE],
 };
->>>>>>> #60 Added fetch field-by-field action
 
 export default {
-  state,
-  mutations,
-  actions,
-  getters
+    state,
+    mutations,
+    actions,
+    getters
 }
