f81ee1ad7b4c7066de6220e0e42d72931363f446
Jaime Tanori <jaime.tanori@gmail.com>
2018-02-16 18:31:37 -0800

    #60 config error fallback (temporal)

--- electron-app/src/renderer/App.vue
+++ electron-app/src/renderer/App.vue
@@ -4,7 +4,10 @@
     <!-- We can easily add a splash screen with this class while the config is loading -->
      <router-view v-if="loaded"></router-view>
      <!-- TODO: Add fail recovery if the API returns an error -->
-     <div v-if="error">error</div>
+     <div v-if="error" class="container">
+       <button class="btn btn-primary" type="button" @click="retry">Retry</button>
+       <p>ERROR: {{ error.message }}</p>
+     </div>
   </div>
 </template>
 
@@ -25,10 +28,29 @@ import {
     },
     async created() {
       await this.$store.dispatch('getConfig');
+    },
+    methods: {
+      async retry() {
+        await this.$store.dispatch('getConfig');
+      }
     }
   }
 </script>
 
 <style>
-  @import "./assets/styles/custom.scss";
+@import "./assets/styles/custom.scss";
+</style>
+
+<style scoped>
+.container {
+  padding: 2rem;
+  text-align: center;
+}
+
+button {
+  border: 0;
+  padding: 1rem;
+  background: #eeeeaa;
+  border-radius: 0.5rem;
+}
 </style>
