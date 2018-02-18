10a71e03712a9b746b19c1403d344e27a212240f
Jaime Tanori <jaime.tanori@gmail.com>
2018-02-18 13:44:04 -0800

    #60 mock global components

--- electron-app/test/unit/index.js
+++ electron-app/test/unit/index.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 Vue.config.devtools = false
 Vue.config.productionTip = false
 
+// mock component
+const routerView = {
+    name: 'router-view',
+    render: h => h('div'),
+};
+
+// register mock components
+Vue.component('router-view', routerView);
+// TODO: Find a better way to register a dinamically rendered component
+// to prevent avoid (bad) karma warnings
+// Vue.component('router-link', ¿?¿?¿?);
+
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context('./specs', true, /\.spec$/)
 testsContext.keys().forEach(testsContext)
