ab80f1c23ba337e3648d3deb6a97ece48b763513
Jaime Tanori <jaime.tanori@gmail.com>
2018-03-02 05:43:07 -0700

    #91 Sort by error count

--- electron-app/src/renderer/components/pages/ErrorByErrorReport.vue
+++ electron-app/src/renderer/components/pages/ErrorByErrorReport.vue
@@ -6,7 +6,8 @@ table.p-table.p-table--full(js-stacktable)
         tr
             td #
             td Explanation
-            td Count
+            td
+                a(@click.prevent="toggleSort('count')" href="#") Count
 
     tbody
         tr(js-modal data-mfp-src='#modal-1' v-for="(item, i) in items")
@@ -17,6 +18,7 @@ table.p-table.p-table--full(js-stacktable)
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import _ from 'lodash'
 import {
     SUBMISSION_ERRORS,
     SUBMISSIONS_REQUEST,
@@ -36,15 +38,44 @@ export default {
             }
 
             return 'N/A'
+        },
+        // TODO: Create sort directive
+        toggleSort(name) {
+            const { sort } = this;
+
+            if (sort[name]) {
+                if (sort[name] === 'desc') {
+                    this.sort[name] = 'asc'
+                } else {
+                    this.sort[name] = 'desc'
+                }
+            }
+
+            return false
+        }
+    },
+    data() {
+        return {
+            sort: {
+                count: 'desc'
+            }
         }
     },
     computed: {
         ...mapGetters({
-            items: SUBMISSION_ERRORS,
             loading: SUBMISSIONS_REQUEST,
             error: SUBMISSIONS_FAILURE,
             filters: FILTERS_META,
-        })
+        }),
+        items() {
+            const items = this.$store.getters[SUBMISSION_ERRORS]
+
+            if (this.sort.count === 'desc') {
+                return _.sortBy(items, ['count']).reverse()
+            } else {
+                return _.sortBy(items, ['count'])
+            }
+        }
     },
     async created() {
         await this.fetchErrors(this.$route.params.id)
@@ -55,3 +86,10 @@ export default {
 <style lang="sass" scoped>
 @import "../../assets/styles/app.sass";
 </style>
+
+<style scoped>
+a:hover {
+    cursor: pointer;
+}
+</style>
+
