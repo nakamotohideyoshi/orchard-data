ae2becaac6f86aa90ad73858aaa8556779374d84
Jaime Tanori <jaime.tanori@gmail.com>
2018-03-10 21:23:03 -0700

    #29 Add empty state and modal dynamic component

---
+++ electron-app/src/renderer/components/sections/EmptyState.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.empty-state
+    .empty-state__title(v-if="title") {{ title }}
+    .empty-state__message(v-if="message") {{ message }}
+</template>
+
+<script>
+export default {
+    name: 'empty-state',
+    props: {
+        title: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        message: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '~@/assets/styles/utilities/_variables';
+
+.empty-state {
+    border-radius: 0.25rem;
+    border: 1px solid;
+    border-color: $borderColor;
+    text-align: center;
+    padding: 0.5rem;
+    margin: 1rem auto;
+
+    &__title {
+        color: $fontColor;
+        font-weight: bold;
+    }
+
+    &__message {
+        color: $fontColor;
+        font-size: 1rem;
+    }
+}
+</style>
+
+
---
+++ electron-app/src/renderer/components/sections/FieldModal.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+include ../pages/_mixins
+modal.modal(
+    height="auto"
+    name="field-modal"
+    @before-open="beforeOpen"
+    @before-close="beforeClose"
+)
+    button.close-button(@click="close")
+        +icon('ico-close')
+    label Description
+    .description {{ getDescription(item.criteria) }}
+    label Row Id in field-by-field report
+    .description {{ item.id }}
+    label Row id in Dataset
+    .description {{ item.rowid }}
+    label Data fields
+    //
+        .description {{detailData.test_data_field_ids}}
+    //
+        .description {{detailData.test_data_field_values}}
+    .btn-group
+        router-link(:to="`/csv/${item.rowid}`").btn.btn-view-detail View Row
+        button(v-on:click="showParams()").btn.btn-view-detail View Test Paramters
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import {
+  FILTERS_META
+} from '@/constants/types'
+
+export default {
+    name: 'FieldModal',
+    data() {
+        return {
+            item: {}
+        }
+    },
+    computed: {
+        ...mapGetters({
+            filters: FILTERS_META
+        })
+    },
+    methods: {
+        beforeOpen(event) {
+            this.item = Object.assign({}, this.item, { ...event.params })
+        },
+        beforeClose(event) {
+            console.log('before close')
+        },
+        getDescription (id) {
+            if (this.filters && this.filters[id] && this.filters[id].orchardDescription) {
+                return this.filters[id].orchardDescription
+            }
+
+            return 'N/A'
+        },
+        showParams() {
+            console.log('show params')
+        },
+        close() {
+            this.$modal.hide('field-modal')
+        }
+    }
+}
+</script>
