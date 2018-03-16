09cf30133357acb27826c349500a0806f535e24a
Victor Nogueira <felladrin@gmail.com>
2018-03-16 19:19:19 -0300

    Apply eslint autofix on 'dev-scripts' directory

--- analysis-lib/dev-scripts/tsv-to-json.js
+++ analysis-lib/dev-scripts/tsv-to-json.js
@@ -1,43 +1,39 @@
-let analysisLibModule = require('../analysis-lib-module');
-let argv = require('minimist')(process.argv.splice(2));
-let fs = require("fs");
+let analysisLibModule = require('../analysis-lib-module')
+let argv = require('minimist')(process.argv.splice(2))
+let fs = require('fs')
 
-let IO = new analysisLibModule.IO();
+let IO = new analysisLibModule.IO()
 
-let DATABASE = analysisLibModule.constants.DATABASE;
-let dbInfo = analysisLibModule.dbInfo[DATABASE].tables;
-let table = 'orchard_dataset_contents';
+let DATABASE = analysisLibModule.constants.DATABASE
+let dbInfo = analysisLibModule.dbInfo[DATABASE].tables
+let table = 'orchard_dataset_contents'
 
-let inputDir = ['.', 'data-tests', 'input-files'];
-let filter = argv['filter'];
-let inputFile = argv['input'] || 'test.tsv';
+let inputDir = ['.', 'data-tests', 'input-files']
+let filter = argv['filter']
+let inputFile = argv['input'] || 'test.tsv'
 
-let inputPath = inputDir.concat(filter).concat(inputFile).join('/');
+let inputPath = inputDir.concat(filter).concat(inputFile).join('/')
 
-if(!argv['filter']) { throw Error('\n ***** No filter specified. *****\n"'); }
-if(!argv['input']) { console.log('\n ***** No file specified. Using test.tsv *****\n"'); }
+if (!argv['filter']) { throw Error('\n ***** No filter specified. *****\n"') }
+if (!argv['input']) { console.log('\n ***** No file specified. Using test.tsv *****\n"') }
 
-let parsedFile = [];
-let fieldsDict = dbInfo[table].columns_dict;
+let parsedFile = []
+let fieldsDict = dbInfo[table].columns_dict
 
 IO.readTsv(inputPath)
   .then(file => {
-
     file.forEach(row => {
-
-      let parsedRow = {};
+      let parsedRow = {}
 
       Object.keys(row).forEach(key => {
-        if(key in fieldsDict) { parsedRow[fieldsDict[key]] = row[key]; }
-      });
+        if (key in fieldsDict) { parsedRow[fieldsDict[key]] = row[key] }
+      })
 
-      parsedFile.push(parsedRow);
-
-    });
-
+      parsedFile.push(parsedRow)
+    })
   })
   .catch(err => {
-    console.log(inputPath);
-    console.log(err);
+    console.log(inputPath)
+    console.log(err)
   })
-  .finally(() => fs.writeFile(`mocks/raw_${filter}.json`, JSON.stringify(parsedFile), 'utf8'));
+  .finally(() => fs.writeFile(`mocks/raw_${filter}.json`, JSON.stringify(parsedFile), 'utf8'))
