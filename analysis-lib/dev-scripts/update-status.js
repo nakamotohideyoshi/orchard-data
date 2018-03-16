ea2dae5640f0d18dfa3b53f447fd764ffdc08f73
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-16 19:19:19 -0300

    Apply eslint autofix on 'dev-scripts' directory

--- analysis-lib/dev-scripts/update-status.js
+++ analysis-lib/dev-scripts/update-status.js
@@ -1,10 +1,10 @@
-let analysisLibModule = require('../analysis-lib-module');
-let argv = require('minimist')(process.argv.slice(2));
+let analysisLibModule = require('../analysis-lib-module')
+let argv = require('minimist')(process.argv.slice(2))
 
-let datasetId = argv['dataset'] || 1;
-let status = argv['status'] || 1;
+let datasetId = argv['dataset'] || 1
+let status = argv['status'] || 1
 
-let dbInterface = new analysisLibModule.DbInterface();
-dbInterface.init();
+let dbInterface = new analysisLibModule.DbInterface()
+dbInterface.init()
 
-dbInterface.updateDatasetStatus(datasetId, status);
+dbInterface.updateDatasetStatus(datasetId, status)
