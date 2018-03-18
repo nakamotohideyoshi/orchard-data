dfa52ed7ab207a8a34d8c45cf3fa2c054d79d967
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-18 12:18:45 -0300

    Improve `filters-module.js` structure

--- analysis-lib/src/filters/filters-module.js
+++ analysis-lib/src/filters/filters-module.js
@@ -1,47 +1,24 @@
-var filter1 = require('./filter1')
-var filter2 = require('./filter2')
-var filter3 = require('./filter3')
-var filter4 = require('./filter4')
-var filter5 = require('./filter5')
-var filter6 = require('./filter6')
-var filter7 = require('./filter7')
-var filter8 = require('./filter8')
-var filter9 = require('./filter9')
-var filter10 = require('./filter10')
-var filter11 = require('./filter11')
-var filter12 = require('./filter12')
-var filter13 = require('./filter13')
-var filter14 = require('./filter14')
-var filter15 = require('./filter15')
-var filter16 = require('./filter16')
-var filter17 = require('./filter17')
-var filter18 = require('./filter18')
-var albumswithvsandmeets = require('./albumswithvsandmeets')
-var correctlanguagesetatalbumlevel = require('./correctlanguagesetatalbumlevel')
-var nazipropaganda = require('./nazipropaganda')
-var duplicatesthreshold = require('./duplicatesthreshold')
-
 module.exports = {
-  'filter1': filter1,
-  'filter2': filter2,
-  'filter3': filter3,
-  'filter4': filter4,
-  'filter5': filter5,
-  'filter6': filter6,
-  'filter7': filter7,
-  'filter8': filter8,
-  'filter9': filter9,
-  'filter10': filter10,
-  'filter11': filter11,
-  'filter12': filter12,
-  'filter13': filter13,
-  'filter14': filter14,
-  'filter15': filter15,
-  'filter16': filter16,
-  'filter17': filter17,
-  'filter18': filter18,
-  'albumswithvsandmeets': albumswithvsandmeets,
-  'nazipropaganda': nazipropaganda,
-  'correctlanguagesetatalbumlevel': correctlanguagesetatalbumlevel,
-  'duplicatesthreshold': duplicatesthreshold
+  filter1: require('./filter1'),
+  filter2: require('./filter2'),
+  filter3: require('./filter3'),
+  filter4: require('./filter4'),
+  filter5: require('./filter5'),
+  filter6: require('./filter6'),
+  filter7: require('./filter7'),
+  filter8: require('./filter8'),
+  filter9: require('./filter9'),
+  filter10: require('./filter10'),
+  filter11: require('./filter11'),
+  filter12: require('./filter12'),
+  filter13: require('./filter13'),
+  filter14: require('./filter14'),
+  filter15: require('./filter15'),
+  filter16: require('./filter16'),
+  filter17: require('./filter17'),
+  filter18: require('./filter18'),
+  albumswithvsandmeets: require('./albumswithvsandmeets'),
+  correctlanguagesetatalbumlevel: require('./correctlanguagesetatalbumlevel'),
+  nazipropaganda: require('./nazipropaganda'),
+  duplicatesthreshold: require('./duplicatesthreshold')
 }
