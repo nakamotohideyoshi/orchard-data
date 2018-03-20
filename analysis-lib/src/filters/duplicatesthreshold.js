1973f604cda854b70250f863c90f8a716cb64b00
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-19 22:26:21 -0300

    Update `duplicatesthreshold` filter to return an array of occurrences

--- analysis-lib/src/filters/duplicatesthreshold.js
+++ analysis-lib/src/filters/duplicatesthreshold.js
@@ -1,10 +1,17 @@
 'use strict'
 
+const filterMeta = require('./filters-meta').duplicatesthreshold
+
+const defaultErrorType = filterMeta['type']
+const defaultExplanationId = 'default'
+
 /**
  * @param {{metadata: Object, dataset: Array}} dataset
- * @returns {{duplicatesRatio: number, numberOfDuplicates: number, exceeded: boolean}}
+ * @returns {Array<{row_id: number, field: array, value: array, explanation_id: array, error_type: array}>}
  */
 module.exports = function (dataset) {
+  const occurrences = []
+
   // Convert duplicatesThreshold into fraction (percentage) by dividing it by 100.
 
   let duplicatesThreshold = dataset.metadata.duplicates_threshold / 100
@@ -35,6 +42,20 @@ module.exports = function (dataset) {
     }
   })
 
+  dataset.dataset.forEach((row, index) => {
+    if (row.hasOwnProperty('isrc') && row['isrc'].length > 0 && duplicatedISRC.indexOf(row['isrc']) > -1) {
+      const occurrence = {
+        'row_id': index,
+        'field': ['isrc'],
+        'value': [row['isrc']],
+        'explanation_id': [defaultExplanationId],
+        'error_type': [defaultErrorType]
+      }
+
+      occurrences.push(occurrence)
+    }
+  })
+
   let duplicatesRatio = numberOfDuplicates / totalNumberOfTracks
 
   // Rule: Let exceeded be true if the duplicates ratio is equal to or greater than the value selected by the user
@@ -44,9 +65,13 @@ module.exports = function (dataset) {
   // Rule: The REST API for the report summary should be extended to carry the duplicates ratio, the gross number
   // of duplicates, and the exceeded flag.
 
-  return {
+  let reportSummaryExtension = {
     duplicatesRatio: duplicatesRatio,
     numberOfDuplicates: numberOfDuplicates,
     exceeded: exceeded
   }
+
+  console.log(reportSummaryExtension) // TODO: Change REST API to carry this info.
+
+  return occurrences
 }
