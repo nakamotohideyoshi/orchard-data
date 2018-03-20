5e1277b72d42b4cbe43bc4b76d13547ed9985911
Geraldo Ribeiro Franciscani <gfranciscani@gmail.com>
2018-03-20 15:36:20 -0300

    Auto starts Analysis Lib API from Electron App
      * Tries to listen on a port. If it's taken, increments value by one
        and tries again. Currently, it tries from port 3000 - 3099. A
        promise is returned with the port number, if resolved, or with an
        error, if any.
    
      * When Electron App starts, Analysis Lib rest server auto start as
        well. If the Electron App crashes or is closed, the API server is
        closed as well
    
      Resolve #46

--- analysis-lib/api/main.js
+++ analysis-lib/api/main.js
@@ -1,22 +1,13 @@
-// Packages
-let express = require('express')
-let app = express()
-let cors = require('cors')
-let bodyParser = require('body-parser')
-let morgan = require('morgan')
+const startAPI = require('./start-api')
 
-process.on('uncaughtException', function (error) { return console.log(error.stack) })
-
-if (process.env.NODE_ENV !== 'test') { app.use(morgan('combined')) }
-
-app.use(cors())
-
-app.use(bodyParser.json())
-
-app.set('port', process.env.PORT || 3000)
-
-app.use('/', require('./api'))
-
-app.listen(app.get('port'), function () {
-  return console.log('Listening on port ' + app.get('port'))
-})
+startAPI()
+  .then((r) => {
+    if (r) {
+      console.log(`Listening on port ${r}`)
+      return Promise.resolve(r)
+    }
+  })
+  .catch((err) => {
+    console.log(err)
+    return Promise.reject(err)
+  })
