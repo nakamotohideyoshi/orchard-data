d8941e475063aad3fcfd304ae5115b5803db5ba7
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-21 18:10:55 +0000

    Revert "Merge branch 'feature/148-no-user-visible-feedback-on-dataset-submission' into 'master'"
    This reverts merge request !97

--- electron-app/src/renderer/components/pages/ReportProgressPage.vue
+++ electron-app/src/renderer/components/pages/ReportProgressPage.vue
@@ -10,17 +10,21 @@ include _mixins
                     .container
                         .p-container__wrapper
                             .container.container--smaller
-                            
+
+                                router-link(:to="'/submissions'").page-back
+                                    .icon.icon-arrow-back
+                                    span Submissions
+
                                 .p-box.report
                                     .report__tabs.report__tabs--disabled(js-scrollbar)
                                         a(href="#").report__tab.is-active {{ categoryOverallText }}
-                                        a(href="#").report__tab {{ categoryItunesText }}
-                                        a(href="#").report__tab {{ categoryCustomText }}
+                                        a(href="#").report__tab {{ ITUNES_CATEGORY }}
+                                        a(href="#").report__tab {{ CUSTOM_CATEGORY }}
 
                                     // report blank
                                     .report-blank
                                         .report-blank__title Your report is being generated
-                                        .report-blank__description 
+                                        .report-blank__description We will send you an email at <a href="mailto">email@emailaddress.com</a> as soon as the report is ready for review.
                                         .report-blank__image
                                             img(src='~@/assets/img/report-blank.png' srcset='img/report-blank@2x.png 2x')
 
