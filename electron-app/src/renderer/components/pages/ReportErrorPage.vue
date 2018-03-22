41b12edf7b29f90592440a16a2dd1435b962ec98
Omaro Taylor <smart.omarodev@gmail.com>
2018-03-22 12:42:41 -0500

    Handle fails in the back end

---
+++ electron-app/src/renderer/components/pages/ReportErrorPage.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+include _mixins
+#barba-wrapper
+    .page(class=outClass)
+        block header
+            AppHeader
+        .page__content
+            block content
+                .p-container
+                    .container
+                        .p-container__wrapper
+                            .container.container--smaller
+                            
+                                .p-box.report
+                                    .report__tabs.report__tabs--disabled(js-scrollbar)
+                                        a(href="#").report__tab.is-active {{ categoryOverallText }}
+                                        a(href="#").report__tab {{ categoryItunesText }}
+                                        a(href="#").report__tab {{ categoryCustomText }}
+
+                                    // report blank
+                                    .report-blank
+                                        .report-blank__title Dataset failed<br>Is this input file actually a dataset?
+                                        .report-blank__description 
+                                        .report-blank__image
+                                            img(src='~@/assets/img/report-blank.png' srcset='img/report-blank@2x.png 2x')
+
+        block footer
+            AppFooter
+</template>
+
+<script>
+import AppHeader from './Header.vue'
+import AppFooter from './Footer.vue'
+import {
+  CUSTOM_CATEGORY,
+  ITUNES_CATEGORY,
+  OVERALL_CATEGORY
+} from '@/constants/report-category'
+
+export default {
+  name: 'report-error-page',
+  components: {
+    AppHeader,
+    AppFooter
+  },
+
+  data () {
+    return {
+      categoryOverallText: OVERALL_CATEGORY,
+      categoryItunesText: ITUNES_CATEGORY,
+      categoryCustomText: CUSTOM_CATEGORY
+    }
+  },
+
+  methods: {}
+}
+</script>
