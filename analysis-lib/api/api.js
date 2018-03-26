aab69df9388485a14ed8c946b5cb1690d8ecf0b1
Mauricio Bronstein <zeahraider1@gmail.com>
2018-03-25 20:39:53 -0700

    move 'vacount_percent' to  `risk` category
    - The "risk" tab of the report summary should be extended to show the various artists ratio and count, similar to the following comp. (#143)

--- analysis-lib/api/api.js
+++ analysis-lib/api/api.js
@@ -405,12 +405,12 @@ router.get('/report-summary/:datasetId', async (req, res) => {
       'rowid': report[0].rowid,
       'dataset_id': report[0].dataset_id,
       'no_of_rows': report[0].no_of_rows,
-      'vacount_percent': report[0].vacount_percent,
       'category': {
         'risk': {
           'no_of_errors': report[0].no_of_risk_errors,
           'error_percent': report[0].error_risk_percent,
-          'error_score': report[0].error_risk_score
+          'error_score': report[0].error_risk_score,
+          'vacount_percent': report[0].vacount_percent
         },
         'itunes': {
           'no_of_errors': report[0].no_of_itunes_errors,
