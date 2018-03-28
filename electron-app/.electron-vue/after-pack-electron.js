9419b3bcf3c8338abba1cf8f91cc769b720a1b30
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-03-28 20:40:23 -0300

    Use 'execSync' instead of 'spawnSync'

--- electron-app/.electron-vue/after-pack-electron.js
+++ electron-app/.electron-vue/after-pack-electron.js
@@ -1,5 +1,6 @@
+const path = require('path')
 const copyDir = require('copy-dir')
-const {spawnSync, execSync} = require('child_process')
+const {execSync} = require('child_process')
 
 /**
  * Function called after creating the electron app unpacked directory.
@@ -10,24 +11,24 @@ let afterPack = function (context) {
   let analysisLibRelativePath = require('path').join('..', 'analysis-lib')
 
   console.log(`  • building analysis-lib for ${context.electronPlatformName}`)
-  spawnSync('npm', ['install'], {cwd: analysisLibRelativePath})
+  execSync('npm install', {cwd: analysisLibRelativePath})
 
   switch (context.electronPlatformName) {
     case 'linux':
-      spawnSync('npm', ['run', 'build:linux'], {cwd: analysisLibRelativePath})
-      spawnSync('npm', ['run', 'build:linux-sqlite3'], {cwd: analysisLibRelativePath})
+      execSync('npm run build:linux', {cwd: analysisLibRelativePath})
+      execSync('npm run build:linux-sqlite3', {cwd: analysisLibRelativePath})
       console.log(`  • copying analysis-lib to ${context.outDir}`)
-      copyDir.sync(require('path').join('..', 'analysis-lib', 'build', 'linux'), context.outDir)
-      spawnSync('chmod', ['+x', 'analysis-lib'], {cwd: context.outDir})
+      copyDir.sync(path.join('..', 'analysis-lib', 'build', 'linux'), context.outDir)
+      execSync('chmod +x analysis-lib', {cwd: context.outDir})
       break
     case 'mac':
     case 'macos':
     case 'darwin':
-      spawnSync('npm', ['run', 'build:mac'], {cwd: analysisLibRelativePath})
-      spawnSync('npm', ['run', 'build:mac-sqlite3'], {cwd: analysisLibRelativePath})
+      execSync('npm run build:mac', {cwd: analysisLibRelativePath})
+      execSync('npm run build:mac-sqlite3', {cwd: analysisLibRelativePath})
       console.log(`  • copying analysis-lib to ${context.appOutDir}`)
-      copyDir.sync(require('path').join('..', 'analysis-lib', 'build', 'mac'), context.appOutDir)
-      spawnSync('chmod', ['+x', 'analysis-lib'], {cwd: context.appOutDir})
+      copyDir.sync(path.join('..', 'analysis-lib', 'build', 'mac'), context.appOutDir)
+      execSync('chmod +x analysis-lib', {cwd: context.appOutDir})
       break
     case 'windows':
     case 'win':
@@ -35,7 +36,7 @@ let afterPack = function (context) {
       execSync('npm run build:windows', {cwd: analysisLibRelativePath})
       execSync('npm run build:windows-sqlite3', {cwd: analysisLibRelativePath})
       console.log(`  • copying analysis-lib to ${context.appOutDir}`)
-      copyDir.sync(require('path').join('..', 'analysis-lib', 'build', 'windows'), context.appOutDir)
+      copyDir.sync(path.join('..', 'analysis-lib', 'build', 'windows'), context.appOutDir)
       break
   }
 }
