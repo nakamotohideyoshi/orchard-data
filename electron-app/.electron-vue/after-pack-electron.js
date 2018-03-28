336fc017bf3bc6c3fd313a4715dbbe7769d1fa62
Victor Nogueira <felladrin@gmail.com>
2018-03-28 20:03:20 -0300

    Fix execution of 'npm run build' via script, on Windows

--- electron-app/.electron-vue/after-pack-electron.js
+++ electron-app/.electron-vue/after-pack-electron.js
@@ -1,35 +1,42 @@
-const copyDir = require('copy-dir');
-const { spawnSync } = require('child_process');
+const copyDir = require('copy-dir')
+const {spawnSync, execSync} = require('child_process')
+
 /**
  * Function called after creating the electron app unpacked directory.
  * @param {{outDir: string, appOutDir: string, electronPlatformName: string}} context
  * @return {Promise|null}
  */
 let afterPack = function (context) {
-  console.log(`  • building analysis-lib`)
-  spawnSync('npm', ['install'], { cwd: '../analysis-lib' });
+  let analysisLibRelativePath = require('path').join('..', 'analysis-lib')
+
+  console.log(`  • building analysis-lib for ${context.electronPlatformName}`)
+  spawnSync('npm', ['install'], {cwd: analysisLibRelativePath})
 
   switch (context.electronPlatformName) {
     case 'linux':
-      spawnSync('npm', ['run', 'build:linux'], { cwd: '../analysis-lib' });
-      spawnSync('npm', ['run', 'build:linux-sqlite3'], { cwd: '../analysis-lib' });
+      spawnSync('npm', ['run', 'build:linux'], {cwd: analysisLibRelativePath})
+      spawnSync('npm', ['run', 'build:linux-sqlite3'], {cwd: analysisLibRelativePath})
       console.log(`  • copying analysis-lib to ${context.outDir}`)
-      copyDir.sync('../analysis-lib/build/linux', context.outDir);
-      spawnSync('chmod', ['+x', 'analysis-lib'], { cwd: context.outDir });
-      break;
+      copyDir.sync(require('path').join('..', 'analysis-lib', 'build', 'linux'), context.outDir)
+      spawnSync('chmod', ['+x', 'analysis-lib'], {cwd: context.outDir})
+      break
     case 'mac':
-      spawnSync('npm', ['run', 'build:mac'], { cwd: '../analysis-lib' });
-      spawnSync('npm', ['run', 'build:mac-sqlite3'], { cwd: '../analysis-lib' });
+    case 'macos':
+    case 'darwin':
+      spawnSync('npm', ['run', 'build:mac'], {cwd: analysisLibRelativePath})
+      spawnSync('npm', ['run', 'build:mac-sqlite3'], {cwd: analysisLibRelativePath})
       console.log(`  • copying analysis-lib to ${context.appOutDir}`)
-      copyDir.sync('../analysis-lib/build/mac', context.appOutDir);
-      spawnSync('chmod', ['+x', 'analysis-lib'], { cwd: context.appOutDir });
-      break;
+      copyDir.sync(require('path').join('..', 'analysis-lib', 'build', 'mac'), context.appOutDir)
+      spawnSync('chmod', ['+x', 'analysis-lib'], {cwd: context.appOutDir})
+      break
+    case 'windows':
     case 'win':
-      spawnSync('npm', ['run', 'build:windows'], { cwd: '../analysis-lib' });
-      spawnSync('npm', ['run', 'build:windows-sqlite3'], { cwd: '../analysis-lib' });
+    case 'win32':
+      execSync('npm run build:windows', {cwd: analysisLibRelativePath})
+      execSync('npm run build:windows-sqlite3', {cwd: analysisLibRelativePath})
       console.log(`  • copying analysis-lib to ${context.appOutDir}`)
-      copyDir.sync('../analysis-lib/build/windows', context.appOutDir);
-      break;
+      copyDir.sync(require('path').join('..', 'analysis-lib', 'build', 'windows'), context.appOutDir)
+      break
   }
 }
 
