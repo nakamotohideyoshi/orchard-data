483ad2223441e8897d5c0b69c508ad172f9f5822
Mauricio Bronstein <zeahraider1@gmail.com>
2018-03-31 00:29:42 -0700

    add script to calculate `duplicates threshold`

---
+++ analysis-lib/src/scripts/run-duplicatesthreshold.js
@@ -0,0 +1,42 @@
+'use strict'
+
+module.exports = async function (datasetId, metadata) {
+  const Promise = require('bluebird')
+
+  // tools and constants
+  const DATABASE = require('./constants').DATABASE
+
+  const duplicatesThreshold = require('../features/duplicatesthreshold')
+  let duplicatesThresholdInfo = {}
+  // DB modules
+  const dbInfo = require('../db-scripts/db-info')
+  const DbInterfaceModule = require('../db-scripts/db-interface')
+
+  // Initializes DB interface
+  const dbInterface = new DbInterfaceModule()
+  dbInterface.init()
+
+  // Main table
+  const orchardTable = dbInfo[DATABASE]['tables']['orchard_dataset_contents']
+
+  // Loads dataset
+  const dataset = await dbInterface.fetchTsvDataset(datasetId)
+  const noOfRows = dataset.length
+
+  if (noOfRows === 0) { throw new Error(`*** dataset_id ${datasetId} does not exist on table ${orchardTable.name} ***`) }
+
+  try {
+    // Waits for all filters to finish
+    duplicatesThresholdInfo = await new Promise(async (resolve, reject) => {
+      try {
+        console.log(`Various Artists count: `)
+
+        // calculate Various Artists Count
+        const info = await duplicatesThreshold(dataset, metadata)
+        resolve(info)
+      } catch (err) { reject(err) }
+    })
+  } catch (err) { throw err }
+
+  return duplicatesThresholdInfo
+}
