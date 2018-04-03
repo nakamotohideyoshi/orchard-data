9cd06ce67c6ae8d683428bd06f467e3b7238bc76
stellardev <rodpdev@gmail.com>
2018-04-03 06:56:17 -0500

    integrate Analytics

--- electron-app/src/renderer/main.js
+++ electron-app/src/renderer/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VModal from 'vue-js-modal'
+import VueAnalytics from 'vue-analytics'
 
 import App from './App'
 import router from './router'
@@ -9,6 +10,10 @@ import ReportSummaryHeader from '@/components/sections/ReportSummaryHeader'
 import EmptyState from '@/components/sections/EmptyState'
 
 Vue.use(VModal, { dynamic: true })
+Vue.use(VueAnalytics, {
+  id: 'UA-8437411-21',
+  router
+})
 // Vue.use(VModal, { dialog: true })
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
