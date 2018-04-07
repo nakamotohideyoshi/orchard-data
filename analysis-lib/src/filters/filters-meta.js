55d97893378a2a478bd1bf89c6a1b9f659ddd299
Mauricio Bronstein <zeahraider1@gmail.com>
2018-04-06 22:26:48 -0700

    update filter-meta.js
    - added new filter to resolve #207

--- analysis-lib/src/filters/filters-meta.js
+++ analysis-lib/src/filters/filters-meta.js
@@ -632,5 +632,21 @@ module.exports = {
     'explanations': {
       'default': `This artist is in the blacklist.`
     }
+  },
+
+  'variousartistsmisspelled': {
+    'orchardDescription': `Have you learned or tried to learn braille? What is your experience with the tactile`,
+    'programmerDescription': `
+      - Let there be a standardized set of misspellings of the term "Various Artists" for any given language.
+      - Let the language in use be determined by [fixme: setting on the batch or setting in metadata]
+      - It is an error if the primary album-level artist is a misspelling of "Various Artists."
+    `,
+    'userExplanation': `"Various Artists" must be spelled exactly like that.`,
+    'category': 'iTunes',
+    'type': 'error',
+    'basis': 'row',
+    'explanations': {
+      'default': `misspelled "various artists"`
+    }
   }
 }
