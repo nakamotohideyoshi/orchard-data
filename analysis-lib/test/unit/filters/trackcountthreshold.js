ab8ac56e959a91fbba820b8fdefd32ee519c57a6
Mauricio Bronstein <zeahraider1@gmail.com>
2018-04-13 02:33:12 -0700

    update unit test

--- analysis-lib/test/unit/filters/trackcountthreshold.js
+++ analysis-lib/test/unit/filters/trackcountthreshold.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert
+const _ = require('lodash')
 const describe = require('mocha').describe
 const it = require('mocha').it
 
@@ -12,38 +13,37 @@ const filterMeta = require('../../../src/filters/filters-meta')[filterId]
 const defaultErrorType = filterMeta.type
 const defaultExplanationId = 'default'
 
-describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
+describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, function () {
   let report = new ReportModule()
   report.init()
   report.addFilter(filterId)
 
-  let numberOfTestsExpectedToPass = 6
-  let dataset = mocks.albumWithTooManyTracks
-  let length = dataset.length
+  it('should fail: Too many tracks in this album. The most is 14', async () => {
+    let mock = mocks.albumWithTooManyTracks
+    let metadata = mocks.metadata
 
-  for (let trackCountThreshold = 0; trackCountThreshold < length; trackCountThreshold++) {
-    it(`should fail when the album has ${length} tracks and the track count threshold is ${trackCountThreshold}`, () => {
-      let occurrences = filter(dataset, trackCountThreshold)
+    const occurrences = filter(mock, metadata)
 
-      assert.isNotEmpty(occurrences)
+    assert.equal(_.isEmpty(occurrences), false)
 
-      let trackNumberGreaterThanThreshold = trackCountThreshold + 1
-
-      occurrences.forEach((occurrence) => {
-        assert.deepEqual(occurrence.field, ['track_no'])
-        assert.deepEqual(occurrence.value, [trackNumberGreaterThanThreshold.toString()])
-        assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
-        assert.deepEqual(occurrence.error_type, [defaultErrorType])
-
-        trackNumberGreaterThanThreshold++
-      })
+    occurrences.forEach((occurrence, idx) => {
+      switch (idx) {
+        case 0:
+          assert.deepEqual(occurrence.row_id, 15)
+          assert.deepEqual(occurrence.field, ['track_no'])
+          assert.deepEqual(occurrence.value, ['15'])
+          assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
+          assert.deepEqual(occurrence.error_type, [defaultErrorType])
+          break
+      }
     })
-  }
+  })
 
-  for (let trackCountThreshold = length; trackCountThreshold < length + numberOfTestsExpectedToPass; trackCountThreshold++) {
-    it(`should pass when the album has ${length} tracks and the track count threshold is ${trackCountThreshold}`, () => {
-      let occurrences = filter(dataset, trackCountThreshold)
-      assert.isEmpty(occurrences)
-    })
-  }
+  it('should pass: The most is 15', async () => {
+    let mock = mocks.albumWithTooManyTracks
+    let metadata = mocks.metadata1
+
+    const occurrences = filter(mock, metadata)
+    assert.equal(_.isEmpty(occurrences), true)
+  })
 })
