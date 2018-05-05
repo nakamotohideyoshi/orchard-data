556d39dfdffcb9bc192e68b2bc60de2ff6a8fb0d
Victor Nogueira <felladrin@gmail.com>
2018-05-05 02:36:37 -0300

    Allow submissions of .xls/.xlsx files on back-end

--- analysis-lib/src/scripts/IO-module.js
+++ analysis-lib/src/scripts/IO-module.js
@@ -4,27 +4,46 @@ module.exports = function () {
   let csvParser = require('csv-parse')
   let csvWriter = require('csv-write-stream')
   let Promise = require('bluebird')
+  let XLSX = require('xlsx')
 
   this.readTsv = function (inputFile) {
     return new Promise((resolve, reject) => {
       // Checks if file exist
       if (fs.existsSync(inputFile)) {
-        // Creates stream of data
-        let stream = fs.createReadStream(inputFile, { encoding: 'utf-8' })
-        let parser = csvParser({
-          delimiter: '\t',
-          columns: true,
-          quote: '`',
-          relax: true
-        }, (err, data) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(data)
-          }
-        })
-
-        stream.pipe(parser)
+        if (inputFile.toLowerCase().indexOf('.xls') !== -1) {
+          let workbook = XLSX.readFile(inputFile)
+          let firstSheetName = workbook.SheetNames[0]
+          let worksheet = workbook.Sheets[firstSheetName]
+          let xlsConvertedToTsv = XLSX.utils.sheet_to_csv(worksheet, {FS: '\t'})
+          csvParser(xlsConvertedToTsv, {
+            delimiter: '\t',
+            columns: true,
+            quote: '`',
+            relax: true
+          }, (err, data) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(data)
+            }
+          })
+        } else {
+          // Creates stream of data
+          let stream = fs.createReadStream(inputFile, { encoding: 'utf-8' })
+          let parser = csvParser({
+            delimiter: '\t',
+            columns: true,
+            quote: '`',
+            relax: true
+          }, (err, data) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(data)
+            }
+          })
+          stream.pipe(parser)
+        }
       } else {
         reject(new Error('File does not exist'))
       }
