5163757144becb8d1a83898d77ab9b0f0081b005
Mauricio Bronstein <zeahraider1@gmail.com>
2018-05-06 21:42:19 -0700

    Remove script to convert dataset into TSV

--- analysis-lib/src/scripts/report-utils.js
+++ analysis-lib/src/scripts/report-utils.js
@@ -2,32 +2,6 @@ const stats = require('stats-analysis')
 
 module.exports = {
 
-  'datasetByrow': function (dataset, datasetSize) {
-    let headers = []
-
-    // Generate Headers
-    if (datasetSize !== 0) {
-      headers.push('rowID')
-      headers = headers.concat(Object.keys(dataset[0]).map(key => key))
-    }
-
-    // Convert headers to TSV
-    let tsv = headers.join('\t')
-    tsv += '\n'
-
-    // Convert dataset to TSV
-    for (let i = 1; i <= datasetSize; i++) {
-      // Add rowID
-      const data = Object.assign({'rowID': i}, dataset[ i - 1 ])
-      const values = Object.keys(data).map(key => data[key])
-
-      // Mount TSV tsv
-      tsv += values.join('\t')
-      tsv += '\n'
-    }
-    return tsv
-  },
-
   'rowByRow': function (report, datasetSize, category) {
     const filtersMeta = require('../filters/filters-meta')
 
