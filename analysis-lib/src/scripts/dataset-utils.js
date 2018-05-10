333587716904d1e9cd36d53ef7c5602264b29c83
Mauricio Bronstein <zeahraider1@gmail.com>
2018-05-10 06:11:37 -0700

    dataset to TSV conversion script update

--- analysis-lib/src/scripts/dataset-utils.js
+++ analysis-lib/src/scripts/dataset-utils.js
@@ -3,21 +3,21 @@ module.exports = {
 
   'datasetToTSV': function (dataset, datasetSize) {
     let headers = []
+    let tsv = ''
 
     // Generate Headers
     if (datasetSize !== 0) {
-      headers.push('rowID')
+      headers.push('row_id')
       headers = headers.concat(Object.keys(dataset[0]).map(key => key))
+      // Convert headers to TSV
+      tsv = headers.join('\t')
+      tsv += '\n'
     }
-
-    // Convert headers to TSV
-    let tsv = headers.join('\t')
-    tsv += '\n'
 
     // Convert dataset to TSV
     for (let i = 1; i <= datasetSize; i++) {
       // Add rowID
-      const data = Object.assign({'rowID': i}, dataset[ i - 1 ])
+      const data = Object.assign({'row_id': i}, dataset[ i - 1 ])
       const values = Object.keys(data).map(key => data[key])
 
       // Mount TSV tsv
