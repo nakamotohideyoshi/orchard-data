ca8c9940fe05ac39eb6c2f36b2065f751f2de853
Jaime Tanori <jaime.tanori@gmail.com>
2018-05-16 13:19:39 -0700

    File linting

--- analysis-lib/src/scripts/IO-module.js
+++ analysis-lib/src/scripts/IO-module.js
@@ -7,70 +7,69 @@ module.exports = function () {
   let XLSX = require('xlsx')
 
   this.readTsv = function (inputFile) {
-    console.log("readTsv 1",inputFile)
+    console.log('readTsv 1', inputFile)
     return new Promise((resolve, reject) => {
-      console.log("readTsv 2")
+      console.log('readTsv 2')
 
-      if( ! fs.existsSync(inputFile)){
-        console.log("readTsv 3")
+      if (!fs.existsSync(inputFile)) {
+        console.log('readTsv 3')
         reject(new Error('File does not exist'))
         return
       }
 
-      console.log("readTsv 4")
-      let arr = inputFile.split(".")
-      console.log("readTsv 5",arr)
-      let ext = arr[arr.length-1].toLowerCase()
-      console.log("readTsv 6",ext)
-      switch(ext){
+      console.log('readTsv 4')
+      let arr = inputFile.split('.')
+      console.log('readTsv 5', arr)
+      let ext = arr[arr.length - 1].toLowerCase()
+      console.log('readTsv 6', ext)
+      switch (ext) {
+        case 'xls':
+        case 'xlsx':
+          console.log('readTsv 7')
+          let workbook = XLSX.readFile(inputFile)
+          let firstSheetName = workbook.SheetNames[0]
+          let worksheet = workbook.Sheets[firstSheetName]
+          let xlsConvertedToTsv = XLSX.utils.sheet_to_csv(worksheet, {FS: '\t'})
+          csvParser(xlsConvertedToTsv, {
+            delimiter: '\t',
+            columns: true,
+            quote: '`',
+            relax: true
+          }, (err, data) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(data)
+            }
+          })
+          break
 
-        case "xls":
-        case "xlsx":
-        console.log("readTsv 7")
-        let workbook = XLSX.readFile(inputFile)
-        let firstSheetName = workbook.SheetNames[0]
-        let worksheet = workbook.Sheets[firstSheetName]
-        let xlsConvertedToTsv = XLSX.utils.sheet_to_csv(worksheet, {FS: '\t'})
-        csvParser(xlsConvertedToTsv, {
-          delimiter: '\t',
-          columns: true,
-          quote: '`',
-          relax: true
-        }, (err, data) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(data)
-          }
-        })
-        break
-
-        case "tsv":
-        console.log("readTsv 8")
-        // Creates stream of data
-        let stream = fs.createReadStream(inputFile, { encoding: 'utf-8' })
-        let parser = csvParser({
-          delimiter: '\t',
-          columns: true,
-          quote: '`',
-          relax: true
-        }, (err, data) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(data)
-          }
-        })
-        stream.pipe(parser)
-        break
+        case 'tsv':
+          console.log('readTsv 8')
+          // Creates stream of data
+          let stream = fs.createReadStream(inputFile, { encoding: 'utf-8' })
+          let parser = csvParser({
+            delimiter: '\t',
+            columns: true,
+            quote: '`',
+            relax: true
+          }, (err, data) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(data)
+            }
+          })
+          stream.pipe(parser)
+          break
 
         default:
-        console.log("readTsv 9")
-        reject(new Error('File type not supported'))
+          console.log('readTsv 9')
+          reject(new Error('File type not supported'))
       } // end switch
     }) // end Promise
   } // end readTsv
-  
+
   this.writeTsv = function (outPath, data, headers) {
     // Creates stream of data
     let writer = csvWriter({
