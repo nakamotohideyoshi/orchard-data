4b18778432b71408ffa6fe2eaa81bdb79faa955a
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-05-16 07:01:52 -0700

    Fixes #285 - HTTP 500 on processing the monster. Creates #294 - tech debt: unit tests for IO-module.js

--- analysis-lib/src/scripts/IO-module.js
+++ analysis-lib/src/scripts/IO-module.js
@@ -7,49 +7,70 @@ module.exports = function () {
   let XLSX = require('xlsx')
 
   this.readTsv = function (inputFile) {
+    console.log("readTsv 1",inputFile)
     return new Promise((resolve, reject) => {
-      // Checks if file exist
-      if (fs.existsSync(inputFile)) {
-        if (inputFile.toLowerCase().indexOf('.xls') !== -1) {
-          let workbook = XLSX.readFile(inputFile)
-          let firstSheetName = workbook.SheetNames[0]
-          let worksheet = workbook.Sheets[firstSheetName]
-          let xlsConvertedToTsv = XLSX.utils.sheet_to_csv(worksheet, {FS: '\t'})
-          csvParser(xlsConvertedToTsv, {
-            delimiter: '\t',
-            columns: true,
-            quote: '`',
-            relax: true
-          }, (err, data) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(data)
-            }
-          })
-        } else {
-          // Creates stream of data
-          let stream = fs.createReadStream(inputFile, { encoding: 'utf-8' })
-          let parser = csvParser({
-            delimiter: '\t',
-            columns: true,
-            quote: '`',
-            relax: true
-          }, (err, data) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(data)
-            }
-          })
-          stream.pipe(parser)
-        }
-      } else {
-        reject(new Error('File does not exist'))
-      }
-    })
-  }
+      console.log("readTsv 2")
 
+      if( ! fs.existsSync(inputFile)){
+        console.log("readTsv 3")
+        reject(new Error('File does not exist'))
+        return
+      }
+
+      console.log("readTsv 4")
+      let arr = inputFile.split(".")
+      console.log("readTsv 5",arr)
+      let ext = arr[arr.length-1].toLowerCase()
+      console.log("readTsv 6",ext)
+      switch(ext){
+
+        case "xls":
+        case "xlsx":
+        console.log("readTsv 7")
+        let workbook = XLSX.readFile(inputFile)
+        let firstSheetName = workbook.SheetNames[0]
+        let worksheet = workbook.Sheets[firstSheetName]
+        let xlsConvertedToTsv = XLSX.utils.sheet_to_csv(worksheet, {FS: '\t'})
+        csvParser(xlsConvertedToTsv, {
+          delimiter: '\t',
+          columns: true,
+          quote: '`',
+          relax: true
+        }, (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(data)
+          }
+        })
+        break
+
+        case "tsv":
+        console.log("readTsv 8")
+        // Creates stream of data
+        let stream = fs.createReadStream(inputFile, { encoding: 'utf-8' })
+        let parser = csvParser({
+          delimiter: '\t',
+          columns: true,
+          quote: '`',
+          relax: true
+        }, (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(data)
+          }
+        })
+        stream.pipe(parser)
+        break
+
+        default:
+        console.log("readTsv 9")
+        reject(new Error('File type not supported'))
+      } // end switch
+    }) // end Promise
+  } // end readTsv
+  
   this.writeTsv = function (outPath, data, headers) {
     // Creates stream of data
     let writer = csvWriter({
