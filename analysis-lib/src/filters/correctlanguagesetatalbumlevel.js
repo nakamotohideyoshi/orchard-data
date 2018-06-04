046e8cae4c4b3ffd614caf1db3742e514d8437b6
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-06-04 12:19:07 -0300

    Update 'correctlanguagesetatalbumlevel' filter for cases where 'release_meta_language' columns has two or more languages (e.g "Spanish; Castillan")

--- analysis-lib/src/filters/correctlanguagesetatalbumlevel.js
+++ analysis-lib/src/filters/correctlanguagesetatalbumlevel.js
@@ -20,34 +20,50 @@ module.exports = async function (row, idx) {
     'error_type': []
   }
 
-  const fieldsToEvaluate = ['release_name', 'track_name']
-  let releaseLanguageCode = iso6391.getCode(row['release_meta_language'] ? row['release_meta_language'].trim().toLowerCase() : 'english')
-
   const cldFactory = await loadModule()
   const identifier = cldFactory.create(0, 1024)
 
-  fieldsToEvaluate.forEach((field) => {
-    const textToEvaluate = row[field]
-    const findResult = identifier.findLanguage(textToEvaluate)
+  const fieldsToEvaluate = ['release_name', 'track_name']
 
-    if (!findResult.language) return
+  let releaseMetaLanguage = (row['release_meta_language']) ? row['release_meta_language'] : 'english'
 
-    // When it detects the text as latin, we consider it's spanish.
-    if (findResult.language === 'la') findResult.language = 'es'
+  let releaseMetaLanguageArray = [releaseMetaLanguage]
 
-    if (findResult.language !== releaseLanguageCode) {
-      if (findResult.probability > MATCH_THRESHOLD) {
+  if (releaseMetaLanguage.indexOf(';') !== -1) {
+    releaseMetaLanguageArray = releaseMetaLanguage.split(';')
+  } else if (releaseMetaLanguage.indexOf(',') !== -1) {
+    releaseMetaLanguageArray = releaseMetaLanguage.split(',')
+  }
+
+  let matched = false
+
+  releaseMetaLanguageArray.forEach((language) => {
+    let releaseLanguageCode = iso6391.getCode(language)
+
+    fieldsToEvaluate.forEach((field) => {
+      const textToEvaluate = row[field]
+      const findResult = identifier.findLanguage(textToEvaluate)
+
+      // If it has already matched a language, or language could not be detected, we skip the next ones.
+      if (matched || !findResult.language || !releaseLanguageCode) return
+
+      // When it detects the text as latin, we consider it's spanish.
+      if (findResult.language === 'la') findResult.language = 'es'
+
+      if (findResult.language === releaseLanguageCode) {
+        matched = true
+      } else if (findResult.probability > MATCH_THRESHOLD) {
         occurrence.field.push(field)
         occurrence.value.push(textToEvaluate)
         occurrence.explanation_id.push(defaultExplanationId)
         occurrence.error_type.push(defaultErrorType)
       }
-    }
 
-    // Future: show expected language and detected language in score
-    // const frequentResult = identifier.findMostFrequentLanguages(textToEvaluate, 3)
-    // console.log(`Find most frequent languages for text "${textToEvaluate}..."`)
-    // console.log(JSON.stringify(frequentResult))
+      // Future: show expected language and detected language in score
+      // const frequentResult = identifier.findMostFrequentLanguages(textToEvaluate, 3)
+      // console.log(`Find most frequent languages for text "${textToEvaluate}..."`)
+      // console.log(JSON.stringify(frequentResult))
+    })
   })
 
   identifier.dispose()
