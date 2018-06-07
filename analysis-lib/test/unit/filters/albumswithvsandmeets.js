2487b22c37c74f2503e0bf5416ea869a4889c34b
Victor Nogueira <felladrin@gmail.com>
2018-06-07 18:42:46 -0300

    Fix mock and test from `albumswithvsandmeets` filter

--- analysis-lib/test/unit/filters/albumswithvsandmeets.js
+++ analysis-lib/test/unit/filters/albumswithvsandmeets.js
@@ -18,23 +18,21 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
   report.addFilter(filterId)
 
   it('should pass: everything is fine', () => {
-    mocks.shouldPass1.forEach((item, index) => {
-      let occurrence = filter(item, index, mocks.shouldPass1)
-      assert.equal(occurrence, false)
-    })
+    let mock = mocks.shouldPass1
+    let occurrences = filter(mock.dataset)
+    assert.equal(occurrences.length, 0)
   })
 
   it('should pass: no errors in the dataset', () => {
-    mocks.shouldPass2.forEach((item, index) => {
-      let occurrence = filter(item, index, mocks.shouldPass2)
-      assert.equal(occurrence, false)
-    })
+    let mock = mocks.shouldPass2
+    let occurrences = filter(mock.dataset)
+    assert.equal(occurrences.length, 0)
   })
 
   it('should fail: original artists (whose songs are being remixed) identified at album level as Remixer', () => {
-    mocks.shouldFail3.forEach((item, index) => {
-      let occurrence = filter(item, index, mocks.shouldFail3)
-
+    let mock = mocks.shouldFail3
+    let occurrences = filter(mock.dataset)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_remixer'])
       assert.deepEqual(occurrence.value, ['Tortoise'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -43,9 +41,9 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
   })
 
   it('should fail: original artists (whose songs are being remixed) NOT listed at the track level', () => {
-    mocks.shouldFail4.forEach((item, index) => {
-      let occurrence = filter(item, index, mocks.shouldFail4)
-
+    let mock = mocks.shouldFail4
+    let occurrences = filter(mock.dataset)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_artist', 'release_artists_primary_artist'])
       assert.deepEqual(occurrence.value, ['Derrick Carter', 'Derrick Carter'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId, defaultExplanationId])
@@ -54,9 +52,9 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
   })
 
   it('should fail: mixing DJ not listed at the album level identified as primary artist', () => {
-    mocks.shouldFail5.forEach((item, index) => {
-      let occurrence = filter(item, index, mocks.shouldFail5)
-
+    let mock = mocks.shouldFail5
+    let occurrences = filter(mock.dataset)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_primary_artist'])
       assert.deepEqual(occurrence.value, ['Tortoise'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
