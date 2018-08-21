aa2cbb9f0ed8f1f5a354a52e928633a0f3676fa8
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-08-21 20:44:35 -0300

    Get the mock root object instead of mock.dataset, as we're not using 'dataset' property on the mocks in this case

--- analysis-lib/test/unit/filters/remixversioninformation.js
+++ analysis-lib/test/unit/filters/remixversioninformation.js
@@ -19,13 +19,13 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should pass - everything ok', () => {
     let mock = mocks.passEverythingOk
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     assert.equal(occurrences.length, 0)
   })
 
   it('should fail - hyphen delimiter', () => {
     let mock = mocks.failHyphenDelimiter
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -37,7 +37,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - tail begins with square brackets', () => {
     let mock = mocks.failTailBeginsWithSquareBrackets
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -49,7 +49,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - tail begins with paren', () => {
     let mock = mocks.failTailBeginsWithParen
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -61,13 +61,13 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should pass - there is no tail', () => {
     let mock = mocks.passThereIsNoTail
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     assert.equal(occurrences.length, 0)
   })
 
   it('should fail - value is "remix"', () => {
     let mock = mocks.failValueIsRemix
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -79,7 +79,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - value is "mix"', () => {
     let mock = mocks.failValueIsMix
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -91,7 +91,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - value is "remixed"', () => {
     let mock = mocks.failValueIsRemixed
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -103,7 +103,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - value is "mixed"', () => {
     let mock = mocks.failValueIsMixed
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -115,19 +115,19 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should pass - flag value is not delimited by word separators before', () => {
     let mock = mocks.passFlagValueIsNotDelimitedByWordSeparatorsBefore
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     assert.equal(occurrences.length, 0)
   })
 
   it('should pass - flag value is not delimited by word separators after', () => {
     let mock = mocks.passFlagValueIsNotDelimitedByWordSeparatorsAfter
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     assert.equal(occurrences.length, 0)
   })
 
   it('should fail - flag value is in parens', () => {
     let mock = mocks.failFlagValueIsInParens
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -139,7 +139,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - flag value is in square brackets', () => {
     let mock = mocks.failFlagValueIsInSquareBrackets
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -151,7 +151,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - flag value is MiX case', () => {
     let mock = mocks.failFlagValueIsMiXCase
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -163,7 +163,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - track remixer has a value', () => {
     let mock = mocks.failTrackRemixerHasAValue
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     assert.deepEqual(occurrences[1].field, ['track_artist_remixer'])
     assert.deepEqual(occurrences[1].value, ['DJ Foo'])
     assert.deepEqual(occurrences[1].explanation_id, [defaultExplanationId])
@@ -172,7 +172,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - non-remix is the original song', () => {
     let mock = mocks.failNonRemixIsTheOriginalSong
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -185,7 +185,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - original song has parens', () => {
     let mock = mocks.failOriginalSongHasParens
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
@@ -198,7 +198,7 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
 
   it('should fail - identical track titles', () => {
     let mock = mocks.failIdenticalTrackTitles
-    let occurrences = filter(mock.dataset)
+    let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_name'])
       assert.deepEqual(occurrence.value, ['Lorem Ipsum'])
