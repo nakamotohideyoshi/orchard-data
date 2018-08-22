38ba9ade3faa495ccd57f6ffcfde4444dcbf7cb7
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-08-21 21:31:41 -0300

    Fix  `remixversioninformation` unit test to consider the `release_name` instead of the `track_name` where needed

--- analysis-lib/test/unit/filters/remixversioninformation.js
+++ analysis-lib/test/unit/filters/remixversioninformation.js
@@ -27,36 +27,33 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
     let mock = mocks.failHyphenDelimiter
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 1'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum - remix 1'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum - remix 2'])
   })
 
   it('should fail - tail begins with square brackets', () => {
     let mock = mocks.failTailBeginsWithSquareBrackets
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 3'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum [remix 1]'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum [remix 2]'])
   })
 
   it('should fail - tail begins with paren', () => {
     let mock = mocks.failTailBeginsWithParen
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 4'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (remix 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (remix 2)'])
   })
 
   it('should pass - there is no tail', () => {
@@ -69,48 +66,44 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
     let mock = mocks.failValueIsRemix
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 6'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (remix 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (remix 2)'])
   })
 
   it('should fail - value is "mix"', () => {
     let mock = mocks.failValueIsMix
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 7'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (mix 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (mix 2)'])
   })
 
   it('should fail - value is "remixed"', () => {
     let mock = mocks.failValueIsRemixed
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 8'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (remixed 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (remixed 2)'])
   })
 
   it('should fail - value is "mixed"', () => {
     let mock = mocks.failValueIsMixed
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 8'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (mixed 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (mixed 2)'])
   })
 
   it('should pass - flag value is not delimited by word separators before', () => {
@@ -129,71 +122,66 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
     let mock = mocks.failFlagValueIsInParens
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 11'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (mixed 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (mixed 2)'])
   })
 
   it('should fail - flag value is in square brackets', () => {
     let mock = mocks.failFlagValueIsInSquareBrackets
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 12'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (dolor)[remix 1]'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (dolor)[remix 2]'])
   })
 
   it('should fail - flag value is MiX case', () => {
     let mock = mocks.failFlagValueIsMiXCase
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 13'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (MiX 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (MiX 2)'])
   })
 
   it('should fail - track remixer has a value', () => {
     let mock = mocks.failTrackRemixerHasAValue
     let occurrences = filter(mock)
-    assert.deepEqual(occurrences[1].field, ['track_artist_remixer'])
-    assert.deepEqual(occurrences[1].value, ['DJ Foo'])
-    assert.deepEqual(occurrences[1].explanation_id, [defaultExplanationId])
-    assert.deepEqual(occurrences[1].error_type, [defaultErrorType])
+    occurrences.forEach((occurrence) => {
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 14'])
+      assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
+      assert.deepEqual(occurrence.error_type, [defaultErrorType])
+    })
   })
 
   it('should fail - non-remix is the original song', () => {
     let mock = mocks.failNonRemixIsTheOriginalSong
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 15'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (remix 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (remix 2)'])
-    assert.deepEqual(occurrences[2].value, ['Lorem ipsum'])
   })
 
   it('should fail - original song has parens', () => {
     let mock = mocks.failOriginalSongHasParens
     let occurrences = filter(mock)
     occurrences.forEach((occurrence) => {
-      assert.deepEqual(occurrence.field, ['track_name'])
+      assert.deepEqual(occurrence.field, ['release_name'])
+      assert.deepEqual(occurrence.value, ['Consectetur 16'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
-    assert.deepEqual(occurrences[0].value, ['Lorem Ipsum (remix 1)'])
-    assert.deepEqual(occurrences[1].value, ['Lorem Ipsum (remix 2)'])
-    assert.deepEqual(occurrences[2].value, ['Lorem ipsum (bonus unmixed)'])
   })
 
   it('should fail - identical track titles', () => {
