94669dea09945d7e79b191cc777440cbb21452b0
Victor Nogueira <felladrin@gmail.com>
2018-08-27 18:29:47 -0300

    Make 'artist blacklist' filter work on 'dataset' basis

--- analysis-lib/test/unit/filters/artistblacklist.js
+++ analysis-lib/test/unit/filters/artistblacklist.js
@@ -4,7 +4,7 @@ const it = require('mocha').it
 
 const ReportModule = require('../../../src/scripts/report-tool')
 
-const filterId = 'artistblacklist'
+const filterId = require('path').parse(__filename).name
 const mocks = require(`../../../mocks/filters/${filterId}`)
 const filter = require(`../../../src/filters/${filterId}`)
 const filterMeta = require('../../../src/filters/filters-meta')[filterId]
@@ -18,190 +18,225 @@ describe(`should test ${filterId}: ${filterMeta['orchardDescription']}`, () => {
   report.addFilter(filterId)
 
   it('should fail: Release Artist(s)-Primary Artist(s)', () => {
-    mocks.shouldFailReleaseArtistPrimaryArtist.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailReleaseArtistPrimaryArtist.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailReleaseArtistPrimaryArtist
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_primary_artist'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Release Artist(s)-Featuring(s)', () => {
-    mocks.shouldFailReleaseArtistFeaturing.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailReleaseArtistFeaturing.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailReleaseArtistFeaturing
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_featuring'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Release Artist(s)-Remixer(s)', () => {
-    mocks.shouldFailReleaseArtistRemixer.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailReleaseArtistRemixer.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailReleaseArtistRemixer
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_remixer'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Release Artist(s)-Composer(s)', () => {
-    mocks.shouldFailReleaseArtistComposer.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailReleaseArtistComposer.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailReleaseArtistComposer
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_composer'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Release Artist(s)-Orchestra(s)', () => {
-    mocks.shouldFailReleaseArtistOrchestra.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailReleaseArtistOrchestra.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailReleaseArtistOrchestra
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_orchestra'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Release Artist(s)-Ensemble(s)', () => {
-    mocks.shouldFailReleaseArtistEnsemble.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailReleaseArtistEnsemble.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailReleaseArtistEnsemble
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_ensemble'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Release Artist(s)-Conductor(s)', () => {
-    mocks.shouldFailReleaseArtistConductor.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailReleaseArtistConductor.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailReleaseArtistConductor
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_conductor'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Track Artist', () => {
-    mocks.shouldFailTrackArtist.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailTrackArtist.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailTrackArtist
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_artist'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Track Artist(s) - Featuring(s)', () => {
-    mocks.shouldFailTrackArtistFeaturing.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailTrackArtistFeaturing.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailTrackArtistFeaturing
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_artist_featuring'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Track Artist(s) - Remixer(s)', () => {
-    mocks.shouldFailTrackArtistRemixer.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailTrackArtistRemixer.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailTrackArtistRemixer
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_artist_remixer'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Track Artist(s) - Composer(s)', () => {
-    mocks.shouldFailTrackArtistComposer.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailTrackArtistComposer.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailTrackArtistComposer
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_artist_composer'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Track Artist(s) - Orchestra(s)', () => {
-    mocks.shouldFailTrackArtistOrchestra.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailTrackArtistOrchestra.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailTrackArtistOrchestra
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_artist_orchestra'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Track Artist(s) - Ensemble(s)', () => {
-    mocks.shouldFailTrackArtistEnsemble.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailTrackArtistEnsemble.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailTrackArtistEnsemble
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_artist_ensemble'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: Track Artist(s) - Conductor(s)', () => {
-    mocks.shouldFailTrackArtistConductor.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailTrackArtistConductor.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailTrackArtistConductor
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['track_artist_conductor'])
       assert.deepEqual(occurrence.value, ['A BLACKLISTED ARTIST'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: case-insensitive match', () => {
-    mocks.shouldFailCaseInsensitiveMatch.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailCaseInsensitiveMatch.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailCaseInsensitiveMatch
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_primary_artist'])
       assert.deepEqual(occurrence.value, ['a blacklisted artist'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: first word in keyword blacklist is checked', () => {
-    mocks.shouldFailFirstWordInKeywordBlacklistIsChecked.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailFirstWordInKeywordBlacklistIsChecked.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailFirstWordInKeywordBlacklistIsChecked
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_primary_artist'])
       assert.deepEqual(occurrence.value, ['first keyword'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: last word in keyword blacklist is checked', () => {
-    mocks.shouldFailLastWordInKeywordBlacklistIsChecked.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailLastWordInKeywordBlacklistIsChecked.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailLastWordInKeywordBlacklistIsChecked
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_primary_artist'])
       assert.deepEqual(occurrence.value, ['last keyword'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should fail: substring match', () => {
-    mocks.shouldFailSubstringMatch.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldFailSubstringMatch.metadata
-      let occurrence = filter(item, index, metadata)
+    const mock = mocks.shouldFailSubstringMatch
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    occurrences.forEach((occurrence) => {
       assert.deepEqual(occurrence.field, ['release_artists_primary_artist'])
       assert.deepEqual(occurrence.value, ['prefix A BLACKLISTED ARTIST suffix'])
       assert.deepEqual(occurrence.explanation_id, [defaultExplanationId])
       assert.deepEqual(occurrence.error_type, [defaultErrorType])
     })
   })
+
   it('should pass', () => {
-    mocks.shouldPass.dataset.forEach((item, index) => {
-      let metadata = mocks.shouldPass.metadata
-      let occurrence = filter(item, index, metadata)
-      assert.equal(occurrence, false)
-    })
+    const mock = mocks.shouldPass
+    const { dataset, metadata } = mock
+    const occurrences = filter(dataset, metadata)
+    assert.equal(occurrences.length, 0)
   })
 })
