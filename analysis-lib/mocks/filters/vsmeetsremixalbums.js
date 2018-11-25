ed5210d2ef8bb724d7a1d9300d3e45a7f98cfd0b
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-11-25 02:36:29 -0300

    Update logic, mock and tests from 'vsmeetsremixalbums' filter

--- analysis-lib/mocks/filters/vsmeetsremixalbums.js
+++ analysis-lib/mocks/filters/vsmeetsremixalbums.js
@@ -1,125 +1,261 @@
 module.exports = {
-  shouldPass1: {
+  shouldFailCaseOfMeets: {
     dataset: [
       {
-        'release_name': 'Derrick Carter Vs. Tortoise',
-        'release_artists_primary_artist': 'Derrick Carter',
-        'release_artists_remixer': 'Derrick Carter',
-        'format': 'EP',
-        'track_no': '1',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Crazy Dub)",
-        'track_artist': 'Tortoise',
-        'track_artist_remixer': 'Derrick Carter'
-      },
-      {
-        'release_name': 'Derrick Carter Vs. Tortoise',
-        'release_artists_primary_artist': 'Derrick Carter',
-        'release_artists_remixer': 'Derrick Carter',
-        'format': 'EP',
-        'track_no': '2',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Outtake)",
-        'track_artist': 'Tortoise',
-        'track_artist_remixer': 'Derrick Carter'
+        release_name: 'A meets B',
+        track_no: '1',
+        release_artists_primary_artist: 'A|B',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Lorem',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651401',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
       }
     ]
   },
-
-  shouldPass2: {
+  shouldFailCaseOfVs: {
     dataset: [
       {
-        'release_name': 'Derrick Carter Meets Tortoise (PERMUTATION 2)',
-        'release_artists_primary_artist': 'Derrick Carter',
-        'release_artists_remixer': 'Derrick Carter',
-        'format': 'EP',
-        'track_no': '1',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Crazy Dub)",
-        'track_artist': 'Tortoise',
-        'track_artist_remixer': 'Derrick Carter'
-      },
-      {
-        'release_name': 'Derrick Carter Meets Tortoise (PERMUTATION 2)',
-        'release_artists_primary_artist': 'Derrick Carter',
-        'release_artists_remixer': 'Derrick Carter',
-        'format': 'EP',
-        'track_no': '2',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Outtake)",
-        'track_artist': 'Tortoise',
-        'track_artist_remixer': 'Derrick Carter'
+        release_name: 'C VS. D',
+        track_no: '1',
+        release_artists_primary_artist: 'C|D',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Ipsum',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651402',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
       }
     ]
   },
-
-  shouldFail3: {
+  shouldFailDotAfterVs: {
     dataset: [
       {
-        'release_name': 'Derrick Carter Meets Tortoise (PERMUTATION 3)',
-        'release_artists_primary_artist': 'Derrick Carter',
-        'release_artists_remixer': 'Tortoise',
-        'format': 'EP',
-        'track_no': '1',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Crazy Dub)",
-        'track_artist': 'Tortoise',
-        'track_artist_remixer': 'Derrick Carter'
-      },
-      {
-        'release_name': 'Derrick Carter Meets Tortoise (PERMUTATION 3)',
-        'release_artists_primary_artist': 'Derrick Carter',
-        'release_artists_remixer': 'Tortoise',
-        'format': 'EP',
-        'track_no': '2',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Outtake)",
-        'track_artist': 'Tortoise',
-        'track_artist_remixer': 'Derrick Carter'
+        release_name: 'E vs F',
+        track_no: '1',
+        release_artists_primary_artist: 'E|F',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Dolor',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651403',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
       }
     ]
   },
-
-  shouldFail4: {
+  shouldFailPrimaryArtistNotPartAOrPartBOrTheirConcatenationDelimiterIsVs: {
     dataset: [
       {
-        'release_name': 'Derrick Carter Meets Tortoise (PERMUTATION 4)',
-        'release_artists_primary_artist': 'Derrick Carter',
-        'release_artists_remixer': 'Derrick Carter',
-        'format': 'EP',
-        'track_no': '1',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Crazy Dub)",
-        'track_artist': 'Derrick Carter',
-        'track_artist_remixer': 'Derrick Carter'
-      },
-      {
-        'release_name': 'Derrick Carter Meets Tortoise (PERMUTATION 4)',
-        'release_artists_primary_artist': 'Derrick Carter',
-        'release_artists_remixer': 'Derrick Carter',
-        'format': 'EP',
-        'track_no': '2',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Outtake)",
-        'track_artist': 'Derrick Carter',
-        'track_artist_remixer': 'Derrick Carter'
+        release_name: 'G vs. H',
+        track_no: '1',
+        release_artists_primary_artist: 'Does Not Match',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Sit',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651404',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
       }
     ]
   },
-
-  shouldFail5: {
+  shouldPassPrimaryArtistIsPartA: {
     dataset: [
       {
-        'release_name': 'Derrick Carter Meets Tortoise (PERMUTATION 5)',
-        'release_artists_primary_artist': 'Tortoise',
-        'release_artists_remixer': 'Derrick Carter',
-        'format': 'EP',
-        'track_no': '1',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Crazy Dub)",
-        'track_artist': 'Tortoise',
-        'track_artist_remixer': 'Derrick Carter'
-      },
+        release_name: 'I vs. J',
+        track_no: '1',
+        release_artists_primary_artist: 'I',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Amet',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651405',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
+      }
+    ]
+  },
+  shouldPassPrimaryArtistIsPartB: {
+    dataset: [
       {
-        'release_name': 'Derrick Carter Meets Tortoise (PERMUTATION 5)',
-        'release_artists_primary_artist': 'Tortoise',
-        'release_artists_remixer': 'Derrick Carter',
-        'format': 'EP',
-        'track_no': '2',
-        'track_name': "In Sarah, Mencken, Christ, And Beethoven There Were Women And Men (D's Winter Outtake)",
-        'track_artist': 'Tortoise',
-        'track_artist_remixer': 'Derrick Carter'
+        release_name: 'K vs. L',
+        track_no: '1',
+        release_artists_primary_artist: 'L',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Consectateur',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651406',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
+      }
+    ]
+  },
+  shouldFailPrimaryArtistNotPartAOrPartBOrTheirConcatenationDelimiterIsMeets: {
+    dataset: [
+      {
+        release_name: 'M Meets N',
+        track_no: '1',
+        release_artists_primary_artist: 'Does Not Match',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Adipiscing',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651407',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
+      }
+    ]
+  },
+  shouldPassPrimaryArtistIsPartAPartBNoSpacesAroundPipe: {
+    dataset: [
+      {
+        release_name: 'O vs. P',
+        track_no: '1',
+        release_artists_primary_artist: 'O|P',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Elit',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651408',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
+      }
+    ]
+  },
+  shouldPassPrimaryArtistIsPartAPartBWhitespaceAroundPipe: {
+    dataset: [
+      {
+        release_name: 'Q vs. R',
+        track_no: '1',
+        release_artists_primary_artist: 'Q | R',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Sed',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651409',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
+      }
+    ]
+  },
+  shouldPassPrimaryArtistIsPartBPartA: {
+    dataset: [
+      {
+        release_name: 'S vs. T',
+        track_no: '1',
+        release_artists_primary_artist: 'T|S',
+        release_meta_language: 'English',
+        orchard_artist: 'Cicero',
+        artist_country: 'USA',
+        format: 'Full Length',
+        imprint: 'Example Label',
+        genre: 'Music',
+        sub_genre: 'West Coast Rap',
+        label_catalog_number: 'Cat-number-12345',
+        volume: '1',
+        track_name: 'Do',
+        meta_language: 'English',
+        track_artist: 'Cicero',
+        explicit: 'No',
+        isrc: 'QMJYX1651410',
+        p_information: '2018 Viva Data',
+        songwriters: 'Cicero',
+        publishers: 'Viva Data'
       }
     ]
   }
